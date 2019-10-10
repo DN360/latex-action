5a8cacb61a0cdc3f1e8b3b0f95903dbe48c469b4
takeru1205 <b1016036@fun.ac.jp>
2019-10-10 17:02:09 +0900

    [add] add upload codes

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,5 +32,32 @@ GITHUB_TOKEN="$2"
 #texliveonfly -c "$compiler" -a "$args" "$root_file"
 latexmk -c "$root_file"
 
+ls
 
-# create release
+# Only upload to non-draft releases
+#IS_DRAFT=$(jq --raw-output '.release.draft' $GITHUB_EVENT_PATH)
+#if [ "$IS_DRAFT" = true ]; then
+#  echo "This is a draft, so nothing to do!"
+#  exit 0
+#fi
+
+# Prepare the headers
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+CONTENT_LENGTH_HEADER="Content-Length: $(stat -c%s main.pdf)"
+CONTENT_TYPE_HEADER="Content-Type: application/pdf"
+
+# Build the Upload URL from the various pieces
+RELEASE_ID=$(jq --raw-output '.release.id' $GITHUB_EVENT_PATH)
+FILENAME=$(basename main.pdf)
+UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
+echo "$UPLOAD_URL"
+
+# Upload the file
+curl \
+  -sSL \
+  -XPOST \
+  -H "${AUTH_HEADER}" \
+  -H "${CONTENT_LENGTH_HEADER}" \
+  -H "${CONTENT_TYPE_HEADER}" \
+  --upload-file "main.pdf" \
+  "${UPLOAD_URL}"
