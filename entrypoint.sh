576032ff82340862932300a3ec8767e217287bb9
takeru1205 <b1016036@fun.ac.jp>
2019-10-10 14:01:42 +0900

    [fix] entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,4 @@ if [ -n "$working_directory" ]; then
   cd "$working_directory"
 fi
 
-# texliveonfly -c "$compiler" -a "$args" "$root_file"
-
-# latexmk -C main.tex && latexmk main.tex && latexmk -c main.tex
-latexmk -pdfdvi -latex=platex -synctex=1 -e "$dvipdf='dvipdfmx %O %S';$bibtex='pbibtex';$makeindex='mendex';" main.tex
-# latexmk -pdfdvi -latex=platex main.tex
+texliveonfly -c "$compiler" -a "$args" "$root_file"
