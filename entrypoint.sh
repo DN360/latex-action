3c98e4300da281b6fb6e2552c7fe0e85c9c49ed1
takeru1205 <b1016036@fun.ac.jp>
2019-10-10 23:21:41 +0900

    [fix] changed release way

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,22 +51,41 @@ find main.pdf
 #fi
 
 # Prepare the headers
-AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
-CONTENT_LENGTH_HEADER="Content-Length: $(stat -c%s main.pdf)"
-CONTENT_TYPE_HEADER="Content-Type: application/pdf"
+#AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+#CONTENT_LENGTH_HEADER="Content-Length: $(stat -c%s main.pdf)"
+#CONTENT_TYPE_HEADER="Content-Type: application/pdf"
 
 # Build the Upload URL from the various pieces
-RELEASE_ID=$(jq --raw-output '.release.id' $GITHUB_EVENT_PATH)
-FILENAME=$(basename main.pdf)
-UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
-echo "$UPLOAD_URL"
+#RELEASE_ID=$(jq --raw-output '.release.id' $GITHUB_EVENT_PATH)
+#FILENAME=$(basename main.pdf)
+#UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
+#echo "$UPLOAD_URL"
 
 # Upload the file
-curl \
-  -sSL \
-  -XPOST \
-  -H "${AUTH_HEADER}" \
-  -H "${CONTENT_LENGTH_HEADER}" \
-  -H "${CONTENT_TYPE_HEADER}" \
-  --upload-file "main.pdf" \
-  "${UPLOAD_URL}"
+#curl \
+#  -sSL \
+#  -XPOST \
+#  -H "${AUTH_HEADER}" \
+#  -H "${CONTENT_LENGTH_HEADER}" \
+#  -H "${CONTENT_TYPE_HEADER}" \
+#  --upload-file "main.pdf" \
+#  "${UPLOAD_URL}"
+
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/[user]/[repo]/releases \
+-d "
+{
+  \"tag_name\": \"v$GITHUB_SHA\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"v$GITHUB_SHA\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
+# upload built pdf
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/[user]/[repo]/releases/${rel_id}/assets?name=main.pdf\
+  --header 'Content-Type: application/pdf'\
+  --upload-file main.pdf
