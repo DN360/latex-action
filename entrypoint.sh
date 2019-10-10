b3a430ecaaa0c0b7d8a050eaef4110fa0c6efb09
takeru1205 <b1016036@fun.ac.jp>
2019-10-10 14:50:34 +0900

    [feature] to upload main.pdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,3 +30,22 @@ fi
 
 #texliveonfly -c "$compiler" -a "$args" "$root_file"
 latexmk -c "$root_file"
+
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/KONEPITO1205/Graduate_Report/releases \
+-d "
+{
+  \"tag_name\": \"v$GITHUB_SHA\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"v$GITHUB_SHA\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
+# upload built pdf
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/KONEPITO1205/Graduate_Report/releases/${rel_id}/assets?name=main.pdf\
+  --header 'Content-Type: application/pdf'\
+  --upload-file main.pdf
