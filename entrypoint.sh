0b314c589c7481fbd521b339f8c6a96fcf8ce6fb
takeru1205 <b1016036@fun.ac.jp>
2019-10-11 11:11:25 +0900

    [feature] echo other type of release id

--- entrypoint.sh
+++ entrypoint.sh
@@ -99,7 +99,10 @@ CONTENT_LENGTH_HEADER="Content-Length: $(stat -c%s main.pdf)"
 CONTENT_TYPE_HEADER="Content-Type: application/pdf"
 
 # Build the Upload URL from the various pieces
-echo jq --raw-output ".[]" ${GITHUB_EVENT_PATH}
+CHECK_ID=$(jq --raw-output ".[]" ${GITHUB_EVENT_PATH})
+echo "$CHECK_ID"
+CHECK_ID_N=$(jq ".[]" ${GITHUB_EVENT_PATH})
+echo "$CHECK_ID_N"
 RELEASE_ID=$(jq --raw-output ["release.id"] ${GITHUB_EVENT_PATH})
 echo "$RELEASE_ID"
 FILENAME=$(basename main.pdf)
