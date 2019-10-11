aafc458d0aba428fb1d0ce3e8f4ebc51431e4b41
takeru1205 <b1016036@fun.ac.jp>
2019-10-11 17:21:36 +0900

    [feature] cat main.pdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,32 +6,6 @@ root_file="$1"
 GITHUB_TOKEN="$2"
 GITHUB_EVENT_PATH="$3"
 TAG="$4"
-#working_directory="$2"
-#compiler="$3"
-#args="$4"
-#extra_packages="$5"
-#extra_system_packages="$6"
-
-#if [ -n "$extra_system_packages" ]; then
-#  for pkg in $extra_system_packages; do
-#    echo "Install $pkg by apk"
-#    apk --no-cache add "$pkg"
-#  done
-#fi
-
-#if [ -n "$extra_packages" ]; then
-#  tlmgr update --self
-#  for pkg in $extra_packages; do
-#    echo "Install $pkg by tlmgr"
-#    tlmgr install "$pkg"
-#  done
-#fi
-
-#if [ -n "$working_directory" ]; then
-#  cd "$working_directory"
-#fi
-
-#texliveonfly -c "$compiler" -a "$args" "$root_file"
 
 latexmk -pdfdvi -latex=platex -synctex=1 -e main.tex
 
@@ -64,6 +38,7 @@ RELEASE_URL="https://uploads.github.com/repos/${REPOSITORY}/releases/${RELEASE_I
 
 # Uploads artifacts
 FILE="/github/workspace/main.pdf"
+cat "${FILE}"
 MIME=$(file -b --mime-type "${FILE}")
 echo "Uploading assets ${FILE} as ${MIME}..."
 NAME=$(basename "${FILE}")
